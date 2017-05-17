5b219ab1c7af01d23cc7f39cd8918de6f1bf43f0
Chris McClimans <chris@hippiehacker.org>
2017-05-17 13:07:45 +1200

    gke: Drop node_count to 2, remove node_pool

--- gke/modules/cluster/node-pool.tf
+++
@@ -1,7 +0,0 @@
-resource "google_container_node_pool" "cncf" {
-  name               = "${ var.name }"
-  project            = "${ var.project }"
-  zone               = "${ var.zone }"
-  cluster            = "${google_container_cluster.cncf.name}"
-  initial_node_count = "${ var.node_pool_count }"
-}

6eab54d68863dc56a3b427a0375ad52ba5f3730b
Chris McClimans <chris@hippiehacker.org>
2017-05-17 16:55:38 +1200

    add configuration for remote tf state

---
+++ gce/config.tf
@@ -0,0 +1,17 @@
+provider "aws" { }
+provider "gzip" {compressionlevel = "BestCompression"}
+
+# configured via:
+# $ export AWS_ACCESS_KEY_ID="anaccesskey"
+# $ export AWS_SECRET_ACCESS_KEY="asecretkey"
+# $ export AWS_DEFAULT_REGION="us-west-2"
+# https://www.terraform.io/docs/providers/aws/#environment-variables
+
+
+terraform {
+  backend "s3" {
+    bucket = "aws"
+    key    = "aws"
+    region = "ap-southeast-2"
+  }
+}

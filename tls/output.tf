26775c49278eecd0ee7e7c00a054429525a6fe47
dlx <denver@ii.coop>
2017-05-22 08:42:31 +1200

    Add TLS Module

---
+++ tls/output.tf
@@ -0,0 +1,13 @@
+output "ca" { value = "${ tls_self_signed_cert.ca_cert.cert_pem }" }
+
+output "etcd" { value = "${ tls_locally_signed_cert.etcd_cert.cert_pem }" }
+output "etcd_key" { value = "${ tls_private_key.etcd_key.private_key_pem }" }
+
+output "apiserver" { value = "${ tls_locally_signed_cert.apiserver_cert.cert_pem}" }
+output "apiserver_key" { value = "${ tls_private_key.apiserver_key.private_key_pem }" }
+
+output "worker" { value = "${ tls_locally_signed_cert.worker_cert.cert_pem}" }
+output "worker_key" { value = "${ tls_private_key.worker_key.private_key_pem}" }
+
+output "client" { value = "${ tls_locally_signed_cert.client_cert.cert_pem }"}
+output "client_key" { value = "${ tls_private_key.client_key.private_key_pem }"}

9341b9b92e5dc48a118610a431d14ca1d158e3a5
dlx <denver@ii.coop>
2017-05-23 04:03:21 +0000

    Always Taint KUBECONFIG on an Apply to Ensure it is on Disk

--- provision.sh
+++ provision.sh
@@ -23,11 +23,9 @@ if [ "$1" = "aws-deploy" ] ; then
               -backend-config 'bucket=aws65972563' \
               -backend-config "key=${TF_VAR_name}" \
               -backend-config 'region=ap-southeast-2'
-
+        terraform taint -module=kubeconfig null_resource.kubeconfig
         time terraform apply ${DIR}/aws
 
-    terraform taint -module=kubeconfig null_resource.kubeconfig
-    terraform apply -target module.kubeconfig.null_resource.kubeconfig
     
     ELB=$(terraform output external_elb)
     export KUBECONFIG=${TF_VAR_data_dir}/kubeconfig

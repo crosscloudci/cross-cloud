cb0ed3270bca7eb6c44ced0fde9d81dfe8f38589
dlx <denver@ii.coop>
2017-06-18 12:30:17 +1200

    Add Certs for GCE

--- gce/modules/worker/cloud-config.tf
+++ gce/modules/worker/cloud-config.tf
@@ -1,3 +1,16 @@
+data "template_file" "kube_proxy" {
+  template = "${ file( "${ path.module }/kube-proxy.yml" )}"
+  vars {
+    hyperkube = "${ var.kubelet_image_url }:${ var.kubelet_image_tag }"
+    name = "${ var.name }"
+    domain = "${ var.domain }"
+  }
+}
+
+resource "gzip_me" "kube_proxy" {
+  input = "${ data.template_file.kube_proxy.rendered }"
+}
+
 resource "gzip_me" "ca" {
   input = "${ var.ca }"
 }
@@ -14,6 +27,8 @@ data "template_file" "cloud-config" {
   template = "${ file( "${ path.module }/cloud-config.yml" )}"
 
   vars {
+
+    fqdn = "${ var.name}-worker${ count.index + 1 }.c.${ var.project }.internal"
     cluster_domain = "${ var.cluster_domain }"
     dns_service_ip = "${ var.dns_service_ip }"
     kubelet_image_url = "${ var.kubelet_image_url }"
@@ -22,8 +37,9 @@ data "template_file" "cloud-config" {
     ca = "${ gzip_me.ca.output }"
     worker = "${ gzip_me.worker.output }"
     worker_key = "${ gzip_me.worker_key.output }"
+    kube_proxy        = "${ gzip_me.kube_proxy.output }"
     name = "${ var.name }"
     domain = "${ var.domain }"
-    # cloud-config = "${ base64encode(var.cloud-config) }"
+    etcd_discovery    = "${ var.etcd_discovery }"
   }
 }

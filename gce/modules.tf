f0010ce6c3d98144446faaa51c98f2622567e65d
dlx <denver@ii.coop>
2017-06-18 19:59:02 +1200

    Use InternalLB

--- gce/modules.tf
+++ gce/modules.tf
@@ -156,8 +156,8 @@ module "tls" {
   tls_apiserver_cert_subject_common_name = "k8s-apiserver"
   tls_apiserver_cert_validity_period_hours = 1000
   tls_apiserver_cert_early_renewal_hours = 100
-  tls_apiserver_cert_dns_names = "kubernetes,kubernetes.default,kubernetes.default.svc,kubernetes.default.svc.cluster.local,*.c.${ var.project }.internal"
-  tls_apiserver_cert_ip_addresses = "127.0.0.1,10.0.0.1,${ module.etcd.external_lb }"
+  tls_apiserver_cert_dns_names = "kubernetes,kubernetes.default,kubernetes.default.svc,kubernetes.default.svc.cluster.local"
+  tls_apiserver_cert_ip_addresses = "127.0.0.1,10.0.0.1,${ module.etcd.external_lb },${ var.internal_lb_ip }"
 
   tls_worker_cert_subject_common_name = "k8s-worker"
   tls_worker_cert_validity_period_hours = 1000

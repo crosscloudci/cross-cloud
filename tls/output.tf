0406feab2bb2ae6ad8506dde67bd5ea074811251
dlx <denver@ii.coop>
2017-07-10 05:59:40 +1200

    Remove Output for ServiceAccount Certs

--- tls/output.tf
+++ tls/output.tf
@@ -12,6 +12,3 @@ output "worker_key" { value = "${ tls_private_key.worker_key.private_key_pem}" }
 output "client" { value = "${ tls_locally_signed_cert.client_cert.cert_pem }"}
 output "client_key" { value = "${ tls_private_key.client_key.private_key_pem }"}
 
-output "serviceaccount" { value = "${ tls_locally_signed_cert.serviceaccount_cert.cert_pem}" }
-output "serviceaccount_key" { value = "${ tls_private_key.serviceaccount_key.private_key_pem}" }
-

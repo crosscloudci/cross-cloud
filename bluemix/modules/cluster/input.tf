38ca7c2d2022e4350bb56b35117062e823c30a65
dlx <denver@ii.coop>
2017-08-27 12:06:01 +1200

    Add bluebix Modules

---
+++ bluemix/modules/cluster/input.tf
@@ -0,0 +1,14 @@
+variable "name" {}
+variable "region" {}
+variable "zone" {}
+variable "project" {}
+variable "node_count" {}
+variable "network" {}
+variable "subnetwork" {}
+variable "node_version" {}
+variable "master_user" {}
+variable "master_password" {}
+variable "node_pool_count" {}
+variable "vm_size" {}
+variable "data_dir" {}
+

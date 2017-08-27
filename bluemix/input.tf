d844dbaee8f5f6eb2649f64027d327a8243b8cad
dlx <denver@ii.coop>
2017-08-27 12:05:19 +1200

    Add Bluemix Input Vars

---
+++ bluemix/input.tf
@@ -0,0 +1,15 @@
+variable "name" { default = "ibm" }
+variable "org" { default = "cncf" }
+
+# variable "region" { default = "us-central1" }
+# variable "zone" { default = "us-central1-a" }
+# variable "project" { default = "test-163823" }
+# variable "cidr" { default = "10.0.0.0/16" }
+# variable "node_count" { default = "3" }
+# variable "node_version" { default = "1.6.2" }
+# variable "master_user" { default = "cncf" }
+# variable "master_password" { default = "demo"}
+# variable "vm_size" { default = "n1-standard-1"}
+# variable "node_pool_count" { default = "3"}
+# variable "data_dir" { default = "/cncf/data" }
+

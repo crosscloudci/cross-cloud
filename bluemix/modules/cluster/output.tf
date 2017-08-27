62375a136212d13109669188c33d7f3f81749511
dlx <denver@ii.coop>
2017-08-27 12:06:01 +1200

    Add bluebix Modules

---
+++ bluemix/modules/cluster/output.tf
@@ -0,0 +1,4 @@
+# output "endpoint" { value = "${ google_container_cluster.cncf.endpoint }" }
+# output "ca" { value = "${ google_container_cluster.cncf.master_auth.0.cluster_ca_certificate }" }
+# output "client" { value = "${ google_container_cluster.cncf.master_auth.0.client_certificate }" }
+# output "client_key" { value = "${ google_container_cluster.cncf.master_auth.0.client_key }" }

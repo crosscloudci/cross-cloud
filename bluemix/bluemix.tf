b5a1859487858fa8d482618dcb02372f8ae38523
dlx <denver@ii.coop>
2017-08-29 09:47:46 +1200

    Use Data Sources for Vlans

--- bluemix/bluemix.tf
+++ bluemix/bluemix.tf
@@ -13,22 +13,36 @@ data "ibm_account" "accountData" {
   org_guid = "${data.ibm_org.orgdata.id}"
 }
 
+data "ibm_network_vlan" "vlan_public" {
+  name = "public"
+}
+
+data "ibm_network_vlan" "vlan_private" {
+  name = "private"
+}
+
 resource "ibm_container_cluster" "testacc_cluster" {
-  name            = "test"
-  datacenter      = "dal10"
-  machine_type    = "free"
-  isolation       = "public"
-  public_vlan_id  = "vlan"
-  private_vlan_id = "vlan"
+  name            = "${ var.name }"
+  datacenter      = "${ var.zone }"
+  machine_type    = "${ var.type }"
+  isolation       = "${ var.isolation }"
+  public_vlan_id  = "${ data.ibm_network_vlan.vlan_public.id }"
+  private_vlan_id = "${ data.ibn_network_vlan.vlan_privateid }"
+  no_subnet       = false
   # subnet_id       = ["1154643"]
+  wait_time_minutes = 10
 
-  workers = [{
-    name = "worker1"
-
-    action = "add"
-  }]
+  workers = ["${join(",", var.worker_nodes)}"]
+  # workers = [{name = "worker1", action = "add"}]
 
   org_guid     = "${ data.ibm_org.orgdata.id }"
   space_guid   = "${ var.name }"
   account_guid = "${ data.ibm_account.accountData.id }"
+}
+
+data "ibm_container_cluster_config" "cluster_config" {
+  cluster_name_id = "${ibm_container_cluster.cluster.name}"
+  org_guid        = "${data.ibm_org.org.id}"
+  space_guid      = "${data.ibm_space.space.id}"
+  account_guid    = "${data.ibm_account.account.id}"
 }

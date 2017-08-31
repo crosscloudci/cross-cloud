9df2619dda2d8e67f2a2ece21a8e8d1a313343ad
dlx <denver@ii.coop>
2017-09-01 06:02:10 +1200

    Remove Old Modules

--- bluemix/modules/vpc/gce-subnet.tf
+++
@@ -1,6 +0,0 @@
-# resource "google_compute_subnetwork" "cncf" {
-#   name          = "${ var.name }"
-#   ip_cidr_range = "${ var.cidr }"
-#   network       = "${ google_compute_network.cncf.self_link }"
-#   region        = "${ var.region }"
-# }

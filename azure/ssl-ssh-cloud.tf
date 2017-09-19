d095e0656010585a9afa44df7a0b156eccef0bc0
dlx <denver@ii.coop>
2017-09-20 05:18:59 +1200

    Only Create SSH Keys

--- azure/ssl-ssh-cloud.tf
+++ azure/ssl-ssh-cloud.tf
@@ -1,25 +1,5 @@
 #Gen Certs and SSH KeyPair
-resource "null_resource" "ssl_gen" {
-
-  provisioner "local-exec" {
-    command = <<EOF
-mkdir -p ${ var.data_dir }/.cfssl 
-${ path.module }/init-cfssl \
-${ var.data_dir }/.cfssl \
-${ azurerm_resource_group.cncf.location } \
-${ var.internal_tld } \
-${ var.k8s_service_ip }
-EOF
-  }
-
-  provisioner "local-exec" {
-    when = "destroy"
-    on_failure = "continue"
-    command = <<EOF
-rm -rf ${ var.data_dir }/.cfssl
-EOF
-  }
-
+resource "null_resource" "ssh_gen" {
 
   provisioner "local-exec" {
     command = <<EOF

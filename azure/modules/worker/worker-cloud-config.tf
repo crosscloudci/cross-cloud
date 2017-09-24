085c2144b2f2c78109f7a714d9d346d1f90bbd62
dlx <denver@ii.coop>
2017-09-25 10:38:43 +1300

    Set Cloud Provider Flag to use Azure

--- azure/modules/worker/worker-cloud-config.tf
+++ azure/modules/worker/worker-cloud-config.tf
@@ -10,6 +10,14 @@ resource "gzip_me" "worker_key" {
   input = "${ var.worker_key }"
 }
 
+resource "gzip_me" "kubeconfig" {
+  input = "${ data.template_file.kubeconfig.rendered }"
+}
+
+resource "gzip_me" "kube_proxy" {
+  input = "${ data.template_file.kube-proxy.rendered }"
+}
+
 data "template_file" "worker_cloud_config" {
   template = "${ file( "${ path.module }/worker-cloud-config.yml" )}"
 
@@ -25,6 +33,16 @@ data "template_file" "worker_cloud_config" {
     ca = "${ gzip_me.ca.output }"
     worker = "${ gzip_me.worker.output }"
     worker_key = "${ gzip_me.worker_key.output }"
+    kube_config = "${ gzip_me.kubeconfig.output }"
+    kube_proxy = "${ gzip_me.kube_proxy.output }"
+  }
+}
+
+data "template_file" "kube-proxy" {
+  template = "${ file( "${ path.module }/kube-proxy.yml" )}"
+
+  vars {
+    master_node = "${ var.name }-master1.${ var.dns_suffix }"
   }
 }
 

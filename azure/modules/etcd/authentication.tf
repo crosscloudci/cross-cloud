407f830ec5c284f5756f6dee80f8137bd05c09e1
dlx <denver@ii.coop>
2017-09-24 13:03:39 +1300

    Don't use Special Chars for Kube Token&Auth files

--- azure/modules/etcd/authentication.tf
+++ azure/modules/etcd/authentication.tf
@@ -10,27 +10,27 @@ resource "gzip_me" "basic_auth_csv" {
 
 resource "random_string" "masters" {
   length = 16
-  special = true
+  special = false
 }
 
 resource "random_string" "kube-controller-manager" {
   length = 16
-  special = true
+  special = false
 }
 
 resource "random_string" "kube-scheduler" {
   length = 16
-  special = true
+  special = false
 }
 
 resource "random_string" "nodes" {
   length = 16
-  special = true
+  special = false
 }
 
 resource "random_string" "kube_proxy" {
   length = 16
-  special = true
+  special = false
 }
 
 data "template_file" "known_tokens_csv" {
@@ -50,7 +50,7 @@ data "template_file" "known_tokens_csv" {
 
 resource "random_string" "masters_auth" {
   length = 16
-  special = true
+  special = false
 }
 
 data "template_file" "basic_auth_csv" {

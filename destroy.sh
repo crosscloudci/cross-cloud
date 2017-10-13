7c21c298e06a47bf3ed53b70f79a4aec6963b891
Chris Hoge <chris@openstack.org>
2017-10-12 22:23:04 -0700

    Initializing the provider, and helper scripts
    Initializing the provider. Some helper scripts. A more
    sane provision entry.

---
+++ destroy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+docker run \
+  -v /tmp/data:/cncf/data \
+  -e OS_AUTH_URL=$OS_AUTH_URL \
+  -e OS_REGION_NAME=$OS_REGION_NAME \
+  -e OS_USER_DOMAIN_NAME=$OS_USER_DOMAIN_NAME \
+  -e OS_USERNAME=$OS_USERNAME \
+  -e OS_TENANT_NAME=$OS_TENANT_NAME \
+  -e OS_PASSWORD=$OS_PASSWORD \
+  -e CLOUD=openstack \
+  -e COMMAND=destroy \
+  -e NAME=cross-cloud \
+  -e BACKEND=file \
+  -ti provisioning
--- openstack/openstack.tf
+++ openstack/openstack.tf
@@ -7,11 +7,16 @@ resource "random_id" "cncf" {
   byte_length = 8
 }
 
+# The OpenStack Provider must be configured through
+# environment variables.
+#
+# One preferred set is:
+# OS_AUTH_URL
+# OS_REGION_NAME
+# OS_USER_DOMAIN_NAME
+# OS_USERNAME
+# OS_TENANT_NAME or OS_TENANT_ID
+# OS_PASSWORD
+
 provider "openstack" {
-  auth_url    = "${ var.os_auth_url }"
-  region      = "${ var.os_region_name }"
-  domain_name = "${ var.os_user_domain_name }"
-  user_name   = "${ var.os_auth_url }"
-  tenant_name = "${ var.os_tenant_name }"
-  password    = "${ var.os_password }"
 }

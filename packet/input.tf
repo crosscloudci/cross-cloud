911685ea7d3c73d99386f559a36ca706285f6709
dlx <denver@ii.coop>
2017-10-17 05:11:28 +1300

    Add Initial Master_Template for Packet Nodes

--- packet/input.tf
+++ packet/input.tf
@@ -1,26 +1,52 @@
 variable "name" { default = "packet" }
 
+variable "data_dir" { default = "/cncf/data/packet" }
+
+
+variable "master_node_count" { default = "3" }
+variable "worker_node_count" { default = "3" }
+
 # Set with env TF_VAR_packet_project_id
 variable "packet_project_id" {} # required for now
 variable "packet_api_key" {}
 # https://www.packet.net/locations/
 variable "packet_facility" { default = "ewr1" }
 variable "packet_billing_cycle" { default = "hourly" }
-variable "packet_operating_system" { default = "coreos_stable" }
-variable "packet_master_device_plan" { default = "baremetal_0" }
-variable "packet_worker_device_plan" { default = "baremetal_0" }
 
-variable "domain" { default = "cncf.packet" }
-variable "data_dir" { default = "/cncf/data/packet" }
 
 # VM Image and size
+variable "packet_master_device_plan" { default = "baremetal_0" }
+variable "packet_worker_device_plan" { default = "baremetal_0" }
+variable "packet_operating_system" { default = "coreos_stable" }
 
+
+# Kubernetes
+variable "cloud_provider" { default = "" }
+variable "cloud_config" { default = "" }
+variable "cluster_domain" { default = "cluster.local" }
+variable "pod_cidr" { default = "100.96.0.0/11" }
+variable "service_cidr"   { default = "100.64.0.0/13" }
+variable "non_masquerade_cidr" { default = "100.64.0.0/10"}
+variable "dns_service_ip" { default = "100.64.0.10" }
 variable "master_node_count" { default = "3" }
 variable "worker_node_count" { default = "3" }
-# Autoscaling not supported by Kuberenetes on Azure yet
-# variable "worker_node_min" { default = "3" }
-# variable "worker_node_max" { default = "5" }
+variable "worker_node_min" { default = "3" }
+variable "worker_node_max" { default = "5" }
+
 
 # Deployment Artifact Versions
 # Hyperkube
 # Set from https://quay.io/repository/coreos/hyperkube
+variable "kubelet_artifact" { default = "https://storage.googleapis.com/kubernetes-release/release/v1.7.2/bin/linux/amd64/kubelet" }
+variable "cni_artifact" { default = "https://github.com/containernetworking/cni/releases/download/v0.5.2/cni-amd64-v0.5.2.tgz" }
+
+variable "etcd_registry" { default = "registry.cncf.ci/etcd/etcd-docker"}
+variable "etcd_tag" { default = "master.38cc76af.26448"}
+variable "kube_apiserver_registry" { default = "gcr.io/google_containers/kube-apiserver"}
+variable "kube_apiserver_tag" { default = "v1.7.2"}
+variable "kube_controller_manager_registry" { default = "gcr.io/google_containers/kube-controller-manager"}
+variable "kube_controller_manager_tag" { default = "v1.7.2"}
+variable "kube_scheduler_registry" { default = "gcr.io/google_containers/kube-scheduler"}
+variable "kube_scheduler_tag" { default = "v1.7.2"}
+variable "kube_proxy_registry" { default = "gcr.io/google_containers/kube-proxy"}
+variable "kube_proxy_tag" { default = "v1.7.2"}

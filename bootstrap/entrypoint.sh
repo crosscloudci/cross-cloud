13e91b9aa3e00ac2422e11623a57092ece2cdc11
dlx <denver@ii.coop>
2017-10-19 13:49:18 +1300

    Source IP Vars

--- bootstrap/entrypoint.sh
+++ bootstrap/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# source /etc/srv/kubernetes/ip.env
+source /etc/srv/kubernetes/ip.env
 
 sed -i "s/PRIVATE_IPV4/$PRIVATE_IPV4/g" ./bash.template
 sed -i "s/PUBLIC_IPV4/$PUBLIC_IPV4/g" ./bash.template

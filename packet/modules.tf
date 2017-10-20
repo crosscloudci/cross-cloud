a1e06683da20266da4533a9c416666fc99a45af6
dlx <denver@ii.coop>
2017-10-21 12:29:40 +1300

    Update Kubernetes Service IP in APICert

--- packet/modules.tf
+++ packet/modules.tf
@@ -46,7 +46,7 @@ module "tls" {
   tls_apiserver_cert_validity_period_hours = 1000
   tls_apiserver_cert_early_renewal_hours = 100
   tls_apiserver_cert_dns_names = "kubernetes,kubernetes.default,kubernetes.default.svc,kubernetes.default.svc.cluster.local,external.skydns.local"
-  tls_apiserver_cert_ip_addresses = "127.0.0.1,10.0.0.1"
+  tls_apiserver_cert_ip_addresses = "127.0.0.1,100.64.0.1"
 
   tls_worker_cert_subject_common_name = "kubernetes-worker"
   tls_worker_cert_validity_period_hours = 1000

eff7121904cd2f79ebe62afac8ca103059b47ed7
dlx <denver@ii.coop>
2017-10-21 11:29:08 +1300

    Set DNS Endpoint for MasterAPI

--- packet/modules.tf
+++ packet/modules.tf
@@ -117,7 +117,7 @@ module "worker_templates" {
   pod_cidr = "${ var.pod_cidr }"
   non_masquerade_cidr = "${ var.non_masquerade_cidr }"
   dns_service_ip = "${ var.dns_service_ip }"
-  internal_lb_ip = ""
+  internal_lb_ip = "external.skydns.local"
 
   ca = "${ module.tls.ca }"
   worker = "${ module.tls.worker }"

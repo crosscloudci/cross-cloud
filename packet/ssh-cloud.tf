12698c815a739232a7c0c3f885c7a8831965ea79
DenverWilliams <denver@debian.nz>
2017-10-28 12:10:53 +1300

    Don't Add SSH Keys to Packet.net

--- packet/ssh-cloud.tf
+++ packet/ssh-cloud.tf
@@ -1,42 +1,25 @@
-#Gen SSH KeyPair
-resource "null_resource" "ssh_gen" {
-
-  provisioner "local-exec" {
-    command = <<EOF
-mkdir -p ${ var.data_dir }/.ssh
-ssh-keygen -t rsa -f ${ var.data_dir }/.ssh/id_rsa -N ''
-EOF
-  }
-
-  provisioner "local-exec" {
-    when = "destroy"
-    on_failure = "continue"
-    command = <<EOF
-    rm -rf ${ var.data_dir }/.ssh
-EOF
-  }
-
-  provisioner "local-exec" {
-    command = <<EOF
-    packet admin -k ${ var.packet_api_key } create-sshkey --label ${ var.name } --file ${ var.data_dir}/.ssh/id_rsa.pub
-EOF
-  }
-
-}
-
-# resource "null_resource" "set_dns" {
+# #Gen SSH KeyPair
+# resource "null_resource" "ssh_gen" {
 
 #   provisioner "local-exec" {
 #     command = <<EOF
-#     echo "${ module.etcd.first_master_ip } endpoint.${ var.name }.${ var.domain }" >> /etc/hosts
-#     EOF
+# mkdir -p ${ var.data_dir }/.ssh
+# ssh-keygen -t rsa -f ${ var.data_dir }/.ssh/id_rsa -N ''
+# EOF
 #   }
 
 #   provisioner "local-exec" {
 #     when = "destroy"
 #     on_failure = "continue"
 #     command = <<EOF
-#     echo FIX ME
+#     rm -rf ${ var.data_dir }/.ssh
 # EOF
 #   }
+
+#   provisioner "local-exec" {
+#     command = <<EOF
+#     packet admin -k ${ var.packet_api_key } create-sshkey --label ${ var.name } --file ${ var.data_dir}/.ssh/id_rsa.pub
+# EOF
+#   }
+
 # }

533ff3572f1af02c17105551633e4f0d556d446f
Chris Hoge <chris@openstack.org>
2017-10-27 08:46:20 -0700

    Basic master node build

--- openstack/modules.tf
+++ openstack/modules.tf
@@ -2,8 +2,8 @@ module "master" {
   source = "./modules/master"
 
   name = "${ var.name }"
-  master_flavor_name = "${ var.flavor_name }"
-  master_image_name = "${ var.image_name }"
+  master_flavor_name = "${ var.master_flavor_name }"
+  master_image_name = "${ var.master_image_name }"
   master_count = "${ var.master_count }"
 }
 

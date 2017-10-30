d3ce20db31c75843694b2bd36170f960e3c1c14f
DenverWilliams <denver@debian.nz>
2017-10-31 11:45:31 +1300

    Echo Nameserver Address into Kubeconfig File

--- packet/resolv.tf
+++ packet/resolv.tf
@@ -3,8 +3,8 @@ resource "null_resource" "resolv_conf" {
 
   provisioner "local-exec" {
     command = <<EOF
-echo -e "nameserver ${ module.master.nameserver }\n$(cat /etc/resolv.conf)" > /etc/resolv.conf
-echo -e "nameserver ${ module.master.nameserver }\n$(cat ${ var.data_dir }/kubeconfig)" >> "${ var.data_dir }/kubeconfig"
+echo -e "nameserver "${ module.master.nameserver }"\n$(cat /etc/resolv.conf)" > /etc/resolv.conf
+echo -e "nameserver "${ module.master.nameserver }"\n$(cat "${ var.data_dir }"/kubeconfig)" >> "${ var.data_dir }"/kubeconfig
 EOF
   }
 

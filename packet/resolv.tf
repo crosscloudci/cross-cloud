7dd999fbef09c5e32d7d876219969903657766a8
DenverWilliams <denver@debian.nz>
2017-10-31 09:43:45 +1300

    Add Packet Nameserver to Kubeconfig

--- packet/resolv.tf
+++ packet/resolv.tf
@@ -4,6 +4,7 @@ resource "null_resource" "resolv_conf" {
   provisioner "local-exec" {
     command = <<EOF
 echo -e "nameserver "${ module.master.nameserver }"\n$(cat /etc/resolv.conf)" > /etc/resolv.conf
+echo -e "nameserver "${ module.master.nameserver }" >> "${ var.data_dir }/kubeconfig"
 EOF
   }
 

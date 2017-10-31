457f55d2b66c8b7d48a75514f6ca46e0bb4b5127
denverwilliams <denver@debian.nz>
2017-10-31 21:41:56 +0000

    ETCD Requires 3 Nodes

--- packet/input.tf
+++ packet/input.tf
@@ -22,7 +22,7 @@ variable "pod_cidr" { default = "10.2.0.0/16" }
 variable "service_cidr"   { default = "10.0.0.0/24" }
 variable "k8s_service_ip" { default = "10.0.0.1" }
 variable "dns_service_ip" { default = "10.0.0.11" }
-variable "master_node_count" { default = "1" }
+variable "master_node_count" { default = "3" }
 variable "worker_node_count" { default = "1" }
 # Autoscaling not supported by Kuberenetes on Azure yet
 # variable "worker_node_min" { default = "3" }

ba0d0146f00f99539b85accf7ee50838741514e2
DenverWilliams <denver@debian.nz>
2017-10-31 15:06:28 +1300

    Split Nameserver Jobs for Packet

--- packet/resolv.tf
+++ packet/resolv.tf
@@ -4,6 +4,15 @@ resource "null_resource" "resolv_conf" {
   provisioner "local-exec" {
     command = <<EOF
 echo -e "nameserver ${ module.master.nameserver }\n$(cat /etc/resolv.conf)" > /etc/resolv.conf
+EOF
+  }
+
+}
+
+resource "null_resource" "resolv_conf_kubeconfig" {
+
+  provisioner "local-exec" {
+    command = <<EOF
 echo -e "#nameserver ${ module.master.nameserver } >> "${ var.data_dir }/kubeconfig"
 EOF
   }

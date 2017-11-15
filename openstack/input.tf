4a08a83e14c8bdb9610ec896432097c99b25c7c1
Chris Hoge <chris@openstack.org>
2017-11-15 11:41:13 -0800

    Setting up K8s installation modules/templates

--- openstack/input.tf
+++ openstack/input.tf
@@ -18,3 +18,30 @@ variable "node_count"       { default = "3" }
 variable "public_network"       { default = "6d6357ac-0f70-4afa-8bd7-c274cc4ea235" }
 variable "private_network_cidr" { default = "192.168.11.0/24" }
 variable "private_lb_ip"        { default = "192.168.11.250" }
+
+# Kubernetes configuration
+variable "etcd_endpoint" {default = "127.0.0.1"}
+variable "cloud_provider" { default = "openstack" }
+variable "cloud_config" { default = "--cloud-config=/etc/srv/kubernetes/cloud-config" }
+variable "cluster_domain" { default = "cluster.local" }
+variable "cluster_name" { default = "kubernetes" }
+variable "pod_cidr" { default = "100.96.0.0/11" }
+variable "service_cidr"   { default = "100.64.0.0/13" }
+variable "non_masquerade_cidr" { default = "100.64.0.0/10"}
+variable "dns_service_ip" { default = "100.64.0.10" }
+
+# Deployment Artifact Versions
+variable "kubelet_artifact" { default = "https://storage.googleapis.com/kubernetes-release/release/v1.8.1/bin/linux/amd64/kubelet" }
+variable "cni_artifact" { default = "https://github.com/containernetworking/cni/releases/download/v0.5.2/cni-amd64-v0.5.2.tgz" }
+
+variable "etcd_image" { default = "gcr.io/google_containers/etcd"}
+variable "etcd_tag" { default = "2.2.1"}
+variable "kube_apiserver_image" { default = "gcr.io/google_containers/kube-apiserver"}
+variable "kube_apiserver_tag"   { default = "v1.8.1"}
+variable "kube_controller_manager_image" { default = "gcr.io/google_containers/kube-controller-manager"}
+variable "kube_controller_manager_tag" { default = "v1.8.1"}
+variable "kube_scheduler_image" { default = "gcr.io/google_containers/kube-scheduler"}
+variable "kube_scheduler_tag" { default = "v1.8.1"}
+variable "kube_proxy_image" { default = "gcr.io/google_containers/kube-proxy"}
+variable "kube_proxy_tag" { default = "v1.8.1"}
+
--- openstack/modules.tf
+++ openstack/modules.tf
@@ -15,7 +15,46 @@ module "master" {
   master_count = "${ var.master_count }"
 
   private_network_id = "${ module.network.private_network_id }"
+}
 
+module "master_templates" {
+  source = "/cncf/master_templates-v1.8.1"
+
+  master_node_count = "${ var.master_count }"
+  name = "${ var.name }"
+  etcd_endpoint = "${ var.etcd_endpoint }"
+
+  kubelet_artifact = "${ var.kubelet_artifact }"
+  cni_artifact = "${ var.cni_artifact }"
+  etcd_image = "${ var.etcd_image }"
+  etcd_tag = "${ var.etcd_tag }"
+  kube_apiserver_image = "${ var.kube_apiserver_image }"
+  kube_apiserver_tag = "${ var.kube_apiserver_tag }"
+  kube_controller_manager_image = "${ var.kube_controller_manager_image }"
+  kube_controller_manager_tag = "${ var.kube_controller_manager_tag }"
+  kube_scheduler_image = "${ var.kube_scheduler_image }"
+  kube_scheduler_tag = "${ var.kube_scheduler_tag }"
+  kube_proxy_image = "${ var.kube_proxy_image }"
+  kube_proxy_tag = "${ var.kube_proxy_tag }"
+
+  cloud_provider = "${ var.cloud_provider }"
+  cloud_config = "${ var.cloud_config }"
+  cluster_domain = "${ var.cluster_domain }"
+  cluster_name = "${ var.cluster_name }"
+  pod_cidr = "${ var.pod_cidr }"
+  service_cidr = "${ var.service_cidr }"
+  non_masquerade_cidr = "${ var.non_masquerade_cidr }"
+  dns_service_ip = "${ var.dns_service_ip }"
+
+  ca = "${ module.tls.ca }"
+  ca_key = "${ module.tls.ca_key }"
+  apiserver = "${ module.tls.apiserver }"
+  apiserver_key = "${ module.tls.apiserver_key }"
+  cloud_config_file = ""
+
+  dns_master = ""
+  dns_conf = ""
+  corefile = ""
 }
 
 module "node" {
@@ -28,6 +67,37 @@ module "node" {
   
   private_network_id = "${ module.network.private_network_id }"
 }
+
+module "worker_templates" {
+  source = "/cncf/worker_templates-v1.8.1"
+
+  worker_node_count = "${ var.node_count }"
+  name = "${ var.name }"
+
+  kubelet_artifact = "${ var.kubelet_artifact }"
+  cni_artifact = "${ var.cni_artifact }"
+  kube_proxy_image = "${ var.kube_proxy_image }"
+  kube_proxy_tag = "${ var.kube_proxy_tag }"
+
+  cloud_provider = "${ var.cloud_provider }"
+  cloud_config = "${ var.cloud_config }"
+  cluster_domain = "${ var.cluster_domain }"
+  pod_cidr = "${ var.pod_cidr }"
+  non_masquerade_cidr = "${ var.non_masquerade_cidr }"
+  dns_service_ip = "${ var.dns_service_ip }"
+  internal_lb_ip = "${ var.private_lb_ip }"
+
+  ca = "${ module.tls.ca }"
+  worker = "${ module.tls.worker }"
+  worker_key = "${ module.tls.worker_key }"
+  cloud_config_file = ""
+
+  dns_worker = ""
+  dns_conf = ""
+  corefile = ""
+  dns_etcd = ""
+}
+
 
 module "tls" {
   source = "../tls"

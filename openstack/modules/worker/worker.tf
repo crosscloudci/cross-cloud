783d3aed88abf90487c69422a8b4eefb4d08a9bd
Chris Hoge <chris@openstack.org>
2017-11-20 10:51:27 -0800

    Worker cloud-init
    Worker cloud-init running.

--- openstack/modules/worker/worker.tf
+++ openstack/modules/worker/worker.tf
@@ -5,7 +5,7 @@ resource "openstack_compute_instance_v2" "cncf" {
   flavor_name = "${ var.worker_flavor_name }"
   security_groups = [ "default", "${ var.security_group_name }" ] # TODO customize security groups
   key_pair = "K8s" # TODO customize keypair
-#  user_data = "${ element(split("`", var.worker_cloud_init), count.index) }"
+  user_data = "${ element(split(",", var.worker_cloud_init), count.index) }"
 
   network {
     uuid = "${ var.internal_network_id }"

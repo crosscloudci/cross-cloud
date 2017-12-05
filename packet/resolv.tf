891c41e81e45ddbd69e94a0cec6c3340c99b7ce9
DenverWilliams <denver@debian.nz>
2017-12-06 08:00:44 +1300

    Remove echo -e for debian based docker images

--- packet/resolv.tf
+++ packet/resolv.tf
@@ -3,7 +3,7 @@ resource "null_resource" "resolv_conf" {
 
   provisioner "local-exec" {
     command = <<EOF
-echo -e "nameserver "${ module.master.nameserver }"\n$(cat /etc/resolv.conf)" > /etc/resolv.conf
+echo "nameserver "${ module.master.nameserver }"\n$(cat /etc/resolv.conf)" > /etc/resolv.conf
 EOF
   }
 
@@ -13,7 +13,7 @@ resource "null_resource" "resolv_conf_kubeconfig" {
 
   provisioner "local-exec" {
     command = <<EOF
-echo -e "#nameserver ${ module.master.nameserver }" >> "${ var.data_dir }/kubeconfig"
+echo "#nameserver ${ module.master.nameserver }" >> "${ var.data_dir }/kubeconfig"
 EOF
   }
 

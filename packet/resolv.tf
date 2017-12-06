528fb78ab81a1a731bded3d9eebef3cbb9537e1e
DenverWilliams <denver@debian.nz>
2017-12-07 12:15:08 +1300

    Merge resolv.conf jobs

--- packet/resolv.tf
+++ packet/resolv.tf
@@ -4,15 +4,6 @@ resource "null_resource" "resolv_conf" {
   provisioner "local-exec" {
     command = <<EOF
 echo "nameserver "${ module.master.nameserver }"\n$(cat /etc/resolv.conf)" > /etc/resolv.conf
-EOF
-  }
-
-}
-
-resource "null_resource" "resolv_conf_kubeconfig" {
-
-  provisioner "local-exec" {
-    command = <<EOF
 echo "#nameserver ${ module.master.nameserver }" | tee -a "${ var.data_dir }/kubeconfig"
 EOF
   }

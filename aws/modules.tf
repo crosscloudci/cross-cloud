a0ec81c4b4c6f1f267c9324ce6704db377d748e3
denverwilliams <denver@debian.nz>
2018-01-17 11:47:36 -0500

    Update aws to support new dns/bootstraping

--- aws/modules.tf
+++ aws/modules.tf
@@ -122,6 +122,7 @@ module "master_templates" {
   master_node_count = "${ var.master_node_count }"
   name = "${ var.name }"
   etcd_endpoint = "${ var.etcd_endpoint }"
+  etcd_bootstrap = ""
 
   kubelet_artifact = "${ var.kubelet_artifact }"
   cni_artifact = "${ var.cni_artifact }"
@@ -153,7 +154,6 @@ module "master_templates" {
 
   dns_master = ""
   dns_conf = ""
-  corefile = ""
 
 }
 
@@ -181,9 +181,6 @@ module "worker_templates" {
   worker_key = "${ module.tls.worker_key }"
   cloud_config_file = ""
 
-  dns_worker = ""
   dns_conf = ""
-  corefile = ""
-  dns_etcd = ""
 
 }

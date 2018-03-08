9c126b1cfee7f60c92aa27215e360625bc6fb7f5
denverwilliams <denver@debian.nz>
2018-03-08 17:00:06 -0500

    Update defaults

--- ibm/input.tf
+++ ibm/input.tf
@@ -1,7 +1,7 @@
-variable "name" { default = "ibm" }
+variable "name" { default = "ibm-cloud" }
 variable "org" { default = "crosscloudci-ibm@vulk.coop" }
 
 variable "zone" { default = "dal10"}
 variable "type" { default = "b1c.4x16"}
 variable "isolation" { default = "public"}
-variable "data_dir" { default = "/cncf/data/bluemix"}
+variable "data_dir" { default = "/cncf/data/ibm"}

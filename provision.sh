1ed92433320b949f161947a014542952c23a1b9b
denverwilliams <denver@debian.nz>
2018-03-14 17:21:54 -0400

    Add cluster verification block for Openstack

--- provision.sh
+++ provision.sh
@@ -200,6 +200,13 @@ elif [[ "$1" = "openstack-deploy" || "$1" = "openstack-destroy" ]] ; then
     elif [ "$1" = "openstack-destroy" ] ; then
         time terraform destroy -force ${DIR}/openstack || true
     fi
+
+    export KUBECONFIG=${TF_VAR_data_dir}/kubeconfig
+    echo "❤ Polling for cluster life - this could take a minute or more"
+    export NODES="$(expr $TF_VAR_master_node_count + $TF_VAR_worker_node_count)"
+    KUBECTL_PATH=$(which kubectl) NUM_NODES="$NODES" KUBERNETES_PROVIDER=local ${DIR}/validate-cluster/cluster/validate-cluster.sh || true
+    _retry "❤ Installing Helm" helm init
+    _retry "Wait for Tiller Deployment to be available" kubectl rollout status -w deployment/tiller-deploy --namespace=kube-system
 # End OpenStack
 
 elif [ "$1" = "packet-deploy" ] ; then

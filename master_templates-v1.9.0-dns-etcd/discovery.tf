a138b56f9894fff3be155566c7fd108ef841e64c
denverwilliams <denver@debian.nz>
2018-03-15 15:12:12 -0400

    Disable etcd token, we are switching to using SRV records for discovery

--- master_templates-v1.9.0-dns-etcd/discovery.tf
+++ master_templates-v1.9.0-dns-etcd/discovery.tf
@@ -1,17 +1,17 @@
-provider "etcdiscovery" {
-}
+# provider "etcdiscovery" {
+# }
 
-resource "etcdiscovery_token" "etcd" {
-  size = "${ var.master_node_count }"
-}
+# resource "etcdiscovery_token" "etcd" {
+#   size = "${ var.master_node_count }"
+# }
 
-output "etcd_discovery" {
-  value = "${ etcdiscovery_token.etcd.id }"
-}
+# output "etcd_discovery" {
+#   value = "${ etcdiscovery_token.etcd.id }"
+# }
 
-resource "etcdiscovery_token" "etcd_events" {
-  size = "${ var.master_node_count }"
-}
+# resource "etcdiscovery_token" "etcd_events" {
+#   size = "${ var.master_node_count }"
+# }
 
 # output "etcd_discovery_events" {
 #   value = "${ etcdiscovery_token.etcd_events.id }"

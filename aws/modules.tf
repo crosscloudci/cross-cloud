1c6594f47f3ccc06499f8c7ba12e3a2a33e8de4a
denverwilliams <denver@debian.nz>
2018-04-17 13:02:22 -0400

    Update aws modules.tf to create certs using the updated tls module & use the v10 k8s templates

--- aws/modules.tf
+++ aws/modules.tf
@@ -80,45 +80,68 @@ module "kubeconfig" {
   endpoint = "master.${ var.name }.${ var.cloud_provider }.local"
   name = "${ var.name }"
   ca = "${ module.tls.ca}"
-  client = "${ module.tls.client }"
-  client_key = "${ module.tls.client_key }"
+  client = "${ module.tls.admin }"
+  client_key = "${ module.tls.admin_key }"
 }
 
 module "tls" {
   source = "../tls"
 
   data_dir = "${ var.data_dir }"
+  master_node_count = "${ var.master_node_count }"
+  worker_node_count = "${ var.worker_node_count }"
 
   tls_ca_cert_subject_common_name = "kubernetes"
-  tls_ca_cert_subject_organization = "Kubernetes"
   tls_ca_cert_subject_locality = "San Francisco"
+  tls_ca_cert_subject_organization = "Kubernetes"
+  tls_ca_cert_subject_organization_unit = "Kubernetes" 
   tls_ca_cert_subject_province = "California"
   tls_ca_cert_subject_country = "US"
   tls_ca_cert_validity_period_hours = 1000
   tls_ca_cert_early_renewal_hours = 100
 
-  tls_client_cert_subject_common_name = "kubecfg"
-  tls_client_cert_validity_period_hours = 1000
-  tls_client_cert_early_renewal_hours = 100
-  tls_client_cert_dns_names = "kubernetes,kubernetes.default,kubernetes.default.svc,kubernetes.default.svc.cluster.local,*.*.compute.internal,*.ec2.internal"
-  tls_client_cert_ip_addresses = "127.0.0.1"
+  tls_admin_cert_subject_common_name = "admin"
+  tls_admin_cert_subject_locality = "San Francisco"
+  tls_admin_cert_subject_organization = "system:masters"
+  tls_admin_cert_subject_organization_unit = "Kubernetes"
+  tls_admin_cert_subject_province = "Callifornia"
+  tls_admin_cert_subject_country = "US"
+  tls_admin_cert_validity_period_hours = 1000
+  tls_admin_cert_early_renewal_hours = 100
+  tls_admin_cert_ip_addresses = "127.0.0.1"
+  tls_admin_cert_dns_names = "kubernetes,kubernetes.default,kubernetes.default.svc,kubernetes.default.svc.cluster.local"
 
-  tls_apiserver_cert_subject_common_name = "kubernetes-master"
-  tls_apiserver_cert_validity_period_hours = 1000
-  tls_apiserver_cert_early_renewal_hours = 100
-  tls_apiserver_cert_dns_names = "kubernetes,kubernetes.default,kubernetes.default.svc,kubernetes.default.svc.cluster.local,*.${ var.name }.${ var.cloud_provider }.local"
-  tls_apiserver_cert_ip_addresses = "127.0.0.1,100.64.0.1,${ var.dns_service_ip }"
+  tls_master_cert_subject_common_name = "system:node:${ var.name }-master"
+  tls_master_cert_subject_common_name_suffix = "${ var.name }.${ var.cloud_provider }.local"
+  tls_master_cert_subject_locality = "San Francisco"
+  tls_master_cert_subject_organization = "system:nodes"
+  tls_master_cert_subject_organization_unit = "Kubernetes"
+  tls_master_cert_subject_province = "California"
+  tls_master_cert_subject_country = "US"
+  tls_master_cert_validity_period_hours = "1000"
+  tls_master_cert_early_renewal_hours = "100"
+  tls_master_cert_validity_period_hours = 1000
+  tls_master_cert_early_renewal_hours = 100
+  tls_master_cert_dns_names = "kubernetes,kubernetes.default,kubernetes.default.svc,kubernetes.default.svc.cluster.local,*.${ var.name }.${ var.cloud_provider }.local"
+  tls_master_cert_ip_addresses = "127.0.0.1,100.64.0.1,${ var.dns_service_ip }"
 
-  tls_worker_cert_subject_common_name = "kubernetes-worker"
+
+  tls_worker_cert_subject_common_name = "system:node:${ var.name }-worker"
+  tls_worker_cert_subject_common_name_suffix = "${ var.name }.${ var.cloud_provider }.local"
+  tls_worker_cert_subject_locality = "San Francisco"
+  tls_worker_cert_subject_organization = "system:nodes"
+  tls_worker_cert_subject_organization_unit = "Kubernetes"
+  tls_worker_cert_subject_province = "California"
+  tls_worker_cert_subject_country = "US"
   tls_worker_cert_validity_period_hours = 1000
   tls_worker_cert_early_renewal_hours = 100
-  tls_worker_cert_dns_names = "kubernetes,kubernetes.default,kubernetes.default.svc,kubernetes.default.svc.cluster.local"
-  tls_worker_cert_ip_addresses = "127.0.0.1"
+  tls_worker_cert_dns_names = "kubernetes,kubernetes.default,kubernetes.default.svc,kubernetes.default.svc.cluster.local,*.${ var.name }.${ var.cloud_provider }.local"
+  tls_worker_cert_ip_addresses = "127.0.0.1,100.64.0.1,${ var.dns_service_ip }"
 
 }
 
 module "master_templates" {
-  source = "/cncf/master_templates-v1.9.0-dns-etcd"
+  source = "/cncf/master_templates-v1.10.0"
 
   master_node_count = "${ var.master_node_count }"
   name = "${ var.name }"
@@ -149,8 +172,8 @@ module "master_templates" {
 
   ca = "${ module.tls.ca }"
   ca_key = "${ module.tls.ca_key }"
-  apiserver = "${ module.tls.apiserver }"
-  apiserver_key = "${ module.tls.apiserver_key }"
+  master = "${ module.tls.master }"
+  master_key = "${ module.tls.master_key }"
   cloud_config_file = ""
 
   dns_conf = "${ module.dns.dns_conf }"
@@ -159,7 +182,7 @@ module "master_templates" {
 }
 
 module "worker_templates" {
-  source = "../worker_templates-v1.9.0"
+  source = "../worker_templates-v1.10.0"
 
   worker_node_count = "${ var.worker_node_count }"
   name = "${ var.name }"

4c549cd910cf16225953650d1c53b7de79e534f8
akutz <akutz@vmware.com>
2018-06-18 11:24:42 -0500

    provider/vSphere: Added deploy/destroy scripts
    This patch adds "deploy.sh" and "destroy.sh" to the vSphere provider.
    These are helpful scripts to quickly deploy and destroy an environment
    with the vSphere provider.

---
+++ vsphere/deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+docker run \
+  --rm \
+  --dns 147.75.69.23 --dns 8.8.8.8 \
+  -v $(pwd)/data:/cncf/data \
+  -e VSPHERE_SERVER=$VSPHERE_SERVER \
+  -e VSPHERE_USER=$VSPHERE_USER \
+  -e VSPHERE_PASSWORD=$VSPHERE_PASSWORD \
+  -e VSPHERE_AWS_ACCESS_KEY_ID=$VSPHERE_AWS_ACCESS_KEY_ID \
+  -e VSPHERE_AWS_SECRET_ACCESS_KEY=$VSPHERE_AWS_SECRET_ACCESS_KEY \
+  -e VSPHERE_AWS_REGION=$VSPHERE_AWS_REGION \
+  -e CLOUD=vsphere \
+  -e COMMAND=deploy \
+  -e NAME=${1:-$(whoami)} \
+  -e BACKEND=file \
+  -ti provisioning

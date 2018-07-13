5ba901ecb884e3898045e9516e2c8f190d3e1b1f
hui luo <luoh@vmware.com>
2018-07-13 15:09:32 -0700

    vSphere: set parameters for non-VMC vSphere
    Adjust the following parameters for non-VMC environment:
    1)  use general resource pool name and network name
    2)  adjust VM cpu, memory size smaller
    3)  set allow unverified server to true

--- vsphere/modules.tf
+++ vsphere/modules.tf
@@ -2,7 +2,7 @@ module "folder" {
   source = "./modules/folder"
 
   datacenter  = "${var.datacenter}"
-  folder_path = "Workloads/CNCF Cross-Cloud/${var.name}"
+  folder_path = "${var.name}"
 }
 
 module "resource_pool" {

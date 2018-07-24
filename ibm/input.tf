daf981cd33cf51eee9a16f1045806c45af580678
denverwilliams <denver@debian.nz>
2018-07-23 21:37:06 -0400

    Fix ibm deprovisioning bug

--- ibm/input.tf
+++ ibm/input.tf
@@ -3,5 +3,5 @@ variable "org" { default = "crosscloudci-ibm@vulk.coop" }
 
 variable "zone" { default = "dal10"}
 variable "type" { default = "b2c.32x128"}
-variable "isolation" { default = "public"}
+variable "isolation" { default = "shared"}
 variable "data_dir" { default = "/cncf/data/ibm"}

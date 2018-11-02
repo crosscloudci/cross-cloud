c0565d730aadd268456b8a01b8c90b8e422ae51b
denverwilliams <denver@debian.nz>
2018-11-02 16:38:14 -0400

    Switch defaults back to coreOS

--- packet/input.tf
+++ packet/input.tf
@@ -20,8 +20,8 @@ variable "packet_billing_cycle" { default = "hourly" }
 
 # VM Image and size
 variable "packet_master_device_plan" { default = "t1.small" }
-variable "packet_worker_device_plan" { default = "c1.xlarge" }
-variable "packet_operating_system" { default = "ubuntu_18_04" }
+variable "packet_worker_device_plan" { default = "c1.small" }
+variable "packet_operating_system" { default = "coreos_stable" }
 
 
 # Kubernetes

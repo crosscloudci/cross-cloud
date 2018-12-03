2049b92387f7f4f57ac70633dce4139cfdb50650
denverwilliams <denver@debian.nz>
2018-12-03 16:45:01 -0500

    Output packet worker public ips as a tmp workaround for https://github.com/cncf/cnfs/issues/152

--- packet/modules/worker/output.tf
+++ packet/modules/worker/output.tf
@@ -1,1 +1,3 @@
 output "worker_ips" { value = "${ join(",", packet_device.workers.*.access_private_ipv4) }" }
+
+output "public_worker_ips" { value = "${ join(",", packet_device.workers.*.access_public_ipv4) }" }

15e297c4fea12c685f7b76441951359c532160c3
denverwilliams <denver@debian.nz>
2019-01-06 16:35:45 -0500

    Update packet.net to use ubuntu to work-around packet.net bug

--- packet/input.tf
+++ packet/input.tf
@@ -21,7 +21,7 @@ variable "packet_billing_cycle" { default = "hourly" }
 # VM Image and size
 variable "packet_master_device_plan" { default = "c1.small.x86" }
 variable "packet_worker_device_plan" { default = "m2.xlarge.x86" }
-variable "packet_operating_system" { default = "coreos_stable" }
+variable "packet_operating_system" { default = "ubuntu_18_04" }
 
 
 # Kubernetes

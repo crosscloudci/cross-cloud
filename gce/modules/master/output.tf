080d93d4bc96eeebc0c30d2a275bb885d1f8fbdd
denverwilliams <denver@debian.nz>
2019-02-18 22:08:47 -0500

    Patch method for getting vm ips

--- gce/modules/master/output.tf
+++ gce/modules/master/output.tf
@@ -1,3 +1,2 @@
-output "master_ips" { value = "${ join(",", google_compute_instance.cncf.*.network_interface.0.address) }" }
-output "public_master_ips" { value = "${ join(",", google_compute_instance.cncf.*.network_interface.0.access_config.0.assigned_nat_ip) }" }
-
+output "master_ips" { value = "${ join(",", google_compute_instance.cncf.*.network_interface.0.network_ip) }" }
+output "public_master_ips" { value = "${ join(",", google_compute_instance.cncf.*.network_interface.0.access_config.0.nat_ip) }" }
--- gce/modules/worker/output.tf
+++ gce/modules/worker/output.tf
@@ -1,1 +1,1 @@
-output "worker_ips" { value = "${ join(",", google_compute_instance.cncf.*.network_interface.0.address) }" }
+output "worker_ips" { value = "${ join(",", google_compute_instance.cncf.*.network_interface.0.network_ip) }" }

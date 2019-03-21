7b6c5dd1e21d63509b2c9779f2c928127cc36d6c
denverwilliams <denver@debian.nz>
2019-03-21 17:12:01 -0400

    Update gitlab job to support arm64 vulk/cncf_ci#129 crosscloudci/crosscloudci#126

--- vsphere/input.tf
+++ vsphere/input.tf
@@ -178,42 +178,15 @@ variable "dns_service_ip" {
 }
 
 # Deployment Artifact Versions
-variable "etcd_artifact" {
-  default = "https://storage.googleapis.com/etcd/v3.2.8/etcd-v3.2.8-linux-amd64.tar.gz"
-}
+variable "etcd_artifact" { default = "https://storage.googleapis.com/etcd/v3.3.11/etcd-v3.3.11-linux-amd64.tar.gz" }
+variable "cni_artifact" { default = "https://github.com/containernetworking/cni/releases/download/v0.6.0/cni-amd64-v0.6.0.tgz" }
+variable "cni_plugins_artifact" { default = "https://github.com/containernetworking/plugins/releases/download/v0.7.4/cni-plugins-amd64-v0.7.4.tgz" }
 
-variable "cni_artifact" {
-  default = "https://github.com/containernetworking/cni/releases/download/v0.6.0/cni-amd64-v0.6.0.tgz"
-}
+variable "kubelet_artifact" { default = "https://storage.googleapis.com/kubernetes-release/release/v1.13.0/bin/linux/amd64/kubelet" }
+variable "kube_apiserver_artifact" { default = "https://storage.googleapis.com/kubernetes-release/release/v1.13.0/bin/linux/amd64/kube-apiserver" }
+variable "kube_controller_manager_artifact" { default = "https://storage.googleapis.com/kubernetes-release/release/v1.13.0/bin/linux/amd64/kube-controller-manager" }
+variable "kube_scheduler_artifact" { default = "https://storage.googleapis.com/kubernetes-release/release/v1.13.0/bin/linux/amd64/kube-scheduler"}
+variable "kube_proxy_artifact" { default = "https://storage.googleapis.com/kubernetes-release/release/v1.13.0/bin/linux/amd64/kube-proxy"}
 
-variable "cni_plugins_artifact" {
-  default = "https://github.com/containernetworking/plugins/releases/download/v0.6.0/cni-plugins-amd64-v0.6.0.tgz"
-}
-
-variable "kubelet_artifact" {
-  default = "https://storage.googleapis.com/kubernetes-release/release/v1.10.1/bin/linux/amd64/kubelet"
-}
-
-variable "kube_apiserver_artifact" {
-  default = "https://storage.googleapis.com/kubernetes-release/release/v1.10.1/bin/linux/amd64/kube-apiserver"
-}
-
-variable "kube_controller_manager_artifact" {
-  default = "https://storage.googleapis.com/kubernetes-release/release/v1.10.1/bin/linux/amd64/kube-controller-manager"
-}
-
-variable "kube_scheduler_artifact" {
-  default = "https://storage.googleapis.com/kubernetes-release/release/v1.10.1/bin/linux/amd64/kube-scheduler"
-}
-
-variable "kube_proxy_artifact" {
-  default = "https://storage.googleapis.com/kubernetes-release/release/v1.10.1/bin/linux/amd64/kube-proxy"
-}
-
-variable "kube_proxy_image" {
-  default = "gcr.io/google_containers/kube-proxy"
-}
-
-variable "kube_proxy_tag" {
-  default = "v1.10.1"
-}
+variable "kube_proxy_image" { default = "gcr.io/google_containers/kube-proxy"}
+variable "kube_proxy_tag" { default = "v1.13.0"}
